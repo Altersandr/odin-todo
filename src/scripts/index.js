b065e1eae683d6b1cc4b1271e9774bd9ccd8d0a8
Oleks <archer.opendub@gmail.com>
2022-12-14 23:35:36 +0100

    made the initial loading page with all the todos stored in the local storage, added the priority button, implemented the ckeched state of the tasks applying a line-through the div container, changed the color pallette of the page, more styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,14 +24,13 @@ overlay.addEventListener("click", closeModal);
 
 
 const projectsContainer = document.querySelector('[data-projects]');
+const navlistContainer = document.querySelector('.navlist');
 const newProjectForm = document.querySelector('[data-new-project-form]');
 const newProjectInput = document.querySelector('[data-new-project-input]');
 const deleteProjectBtn = document.querySelector('[data-delete-project-btn]');
 
-const taskDisplayContainer = document.querySelector('[data-task-display-container]')
+
 const taskTitleElement = document.querySelector('[data-task-title]')
-const taskCountElement = document.querySelector('[data-task-count]')
-const tasksContainer = document.querySelector('[data-tasks]')
 
 const LOCAL_STORAGE_PROJECT_KEY = 'task.projects';
 const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY ='task.selectedProjectId';
@@ -40,12 +39,28 @@ let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY))||[];
 
 let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
 
+
+navlistContainer.addEventListener('click', e=>{
+  if(e.target.tagName.toLowerCase()==='li'){
+    if(e.target.classList = "home"){
+      selectedProjectId = null
+      taskTitleElement.innerHTML = "Home";
+      saveProjects()
+      renderTasks()
+  }else{
+    selectedProjectId = e.target.dataset.projectId
+    saveProjects()
+    renderProjects()
+  }
+  }
+})
+
 projectsContainer.addEventListener('click', e=>{
   if(e.target.tagName.toLowerCase()==='li'){
     selectedProjectId = e.target.dataset.projectId
     saveProjects()
     renderProjects()
-    renderTasks()
+    
   }
 })
 
@@ -57,10 +72,10 @@ deleteProjectBtn.addEventListener('click', e=>{
   selectedProjectId = null;
   saveProjects()
   renderProjects()
-  renderTasks()
+
 })
 
-console.log(tasks)
+
 
 newProjectForm.addEventListener('submit', e=>{
   e.preventDefault();
@@ -71,18 +86,18 @@ newProjectForm.addEventListener('submit', e=>{
   projects.push(project);
   saveProjects()
   renderProjects()
-  renderTasks()
+ 
 })
 
 function createProject (name){
- return {id: Date.now().toString(), name: name, projectTasks: []}
+ return {id: Date.now().toString(), name: name}
 }
 
 
 function saveProjects(){
   localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
   localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId)
-  // localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(taskList))
+
 }
 
 
@@ -91,21 +106,15 @@ function renderProjects (){
   render()
   
   const selectedProject = projects.find(project =>project.id === selectedProjectId)
-  if (selectedProjectId == null){
-    taskDisplayContainer.style.display = "none"
-  }else{
-    taskDisplayContainer.style.display = ""
+  
+    if(selectedProject===undefined){
+      return
     taskTitleElement.innerText = selectedProject.name
-    // renderTaskCount(selectedProject)
-    // clearElement(tasksContainer)
-  }
+    }else{taskTitleElement.innerText = selectedProject.name}
+  
+  renderTasks()
 }
 
-// function renderTaskCount(selectedProject){
-//   const remainingTasks = selectedProject.tasks.filter(task=> !task.complete).length
-//   const taskString =  remainingTasks ===1 ? "task": "tasks"
-//   taskCountElement.innerText  = `${remainingTasks} ${taskString} remaining`
-// }
 
 function render(){
   projects.forEach(project =>{
@@ -121,12 +130,32 @@ function render(){
 }
 
 
-
 function clearElement(element){
   while(element.firstChild){
     element.removeChild(element.firstChild)
   }
 }
+
+window.addEventListener('load', e=>{
+  selectedProjectId = null
+      taskTitleElement.innerHTML = "Home";
+      saveProjects()
+      renderTasks()
+})
+
+
+const taskDetailBtn = document.querySelector('.task-details-btn');
+// taskDetailBtn.addEventListener('click', showDetails)
+
+console.log(taskDetailBtn)
+
+// function showDetails(){
+//   const detailsModal = document.querySelector(".task-details");
+//   detailsModal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// }
+
+
 
 renderProjects()
 
@@ -134,3 +163,24 @@ export {closeModal}
 
 export {selectedProjectId}
 
+export {saveProjects}
+
+
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const openModalBtn = document.querySelector(".btn-open");
+// const closeModalBtn = document.querySelector(".btn-close");
+
+// const openModal = function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//   };
+
+// openModalBtn.addEventListener("click", openModal);
+
+// const closeModal = function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+//   };
+
+// closeModalBtn.addEventListener("click", closeModal);
--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,11 +1,13 @@
 
 class Task {
-    constructor(title, description, dueDate, notes, id){
+    constructor(title, description, dueDate, notes, id, priority){
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.notes = notes;
     this.id = id;
+    this.checked = false;
+    this.priority = priority
     }
 }
 
@@ -16,27 +18,22 @@ function createTask(){
     const dueDate = document.getElementById('date').value
     const notes = document.getElementById('notes').value
     const id = selectedProjectId;
-    return new Task(title, description, dueDate, notes, id)
+    const priority = document.querySelector('input[type=radio]:checked').id;
+    console.log(priority)
+    return new Task(title, description, dueDate, notes, id, priority)
 }
 
 const LOCAL_STORAGE_TASKS_KEY = 'task.list';
 let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_KEY))||[];
 
 
-// const tasks = [];
-    // title: "Brush teeth",
-    // description: "",
-    // dueDate: `2022-12-12`,
-    // priority: "high",
-    // notes: "Use the newly bought toothpaste",
-    // checklist: "checked",
-
-
 export {tasks}
 import { closeModal } from "./index";
 import { selectedProjectId } from "./index";
+import { saveProjects } from "./index";
 
 const addToDo = function(){
+    if(title.value ===""){return}
     const newTask = createTask();
     tasks.push(newTask)
     console.log(tasks)
@@ -53,35 +50,26 @@ const addBtn = document.getElementById('addToDoBtn');
                         
 addBtn.addEventListener('click', addToDo);
 
-// function 
+function deleteTask (e){
+    const taskName = e.target.parentNode.querySelector('span').innerText
 
-// function createDomTask(){
-//     const divContainer = document.createElement('div');
-//     divContainer.classList.add('tasks');
-//     const checkbox = document.createElement('input');
-//     checkbox.type = "checkbox";
-//     checkbox.name = "checkbox";
-//     checkbox.id = "checkbox";
-//     const taskTitle = document.createElement('span');
-//     const taskDueDate = document.createElement('span');
-//     const editPic = document.createElement('img');
-//     editPic.height = "20px";
-//     editPic.src = "../src/img/edit.svg";
-//     const deletePic = document.createElement('img');
-//     deletePic.height = "20px";
-//     deletePic.src="../src/img/trash.svg";
-// };
+    tasks = tasks.filter(task => task.title !== taskName);
+    tasks = localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
+    tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_KEY))||[];
+    renderTasks()
+}
 
 function renderTasks (){
     const mainContainer = document.querySelector('.task-body');
     mainContainer.innerHTML = "";
     tasks.forEach(task=>{
-        
+        const priority = task.priority
+       
         const divContainer = document.createElement('div');
         divContainer.dataset.id =  selectedProjectId
         
         divContainer.classList.add('tasks');
-        
+        divContainer.classList.add(priority)
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
         checkbox.name = "checkbox";
@@ -89,26 +77,53 @@ function renderTasks (){
         const taskTitle = document.createElement('span');
         const taskDueDate = document.createElement('span');
         const taskDetails = document.createElement('span');
-        taskDetails.id = "task-details";
+        taskDetails.className = "task-details-btn";
         const editPic = document.createElement('img');
         editPic.height = "20";
         editPic.src = "../src/img/edit.svg";
         const deletePic = document.createElement('img');
         deletePic.height = "20";
         deletePic.src="../src/img/trash.svg";
-        taskTitle.innerHTML = task.title;
+        deletePic.classList.add("deleteTask");
+        
+        const controls = document.createElement('div');
+        controls.id = 'controls';
+        controls.appendChild(taskDetails)
+        controls.appendChild(editPic)
+        controls.appendChild(deletePic)
+        deletePic.addEventListener('click', deleteTask)
+
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                divContainer.classList.add("checked");
+            } else {
+                divContainer.classList.remove("checked");
+            }
+          });
+
+        taskTitle.innerText = task.title;
         taskDueDate.innerHTML = task.dueDate;
         taskDetails.innerHTML = 'Details';
         divContainer.appendChild(checkbox)
         divContainer.appendChild(taskTitle)
-        divContainer.appendChild(taskDetails)
+        // divContainer.appendChild(taskDetails)
         divContainer.appendChild(taskDueDate)
-        divContainer.appendChild(editPic)
-        divContainer.appendChild(deletePic)
+        divContainer.appendChild(controls)
+        
      if(task.id === selectedProjectId){
-        mainContainer.appendChild(divContainer)}
+        mainContainer.appendChild(divContainer)
+}else if (selectedProjectId ===null){
+    // selectedProjectId = ""
+    mainContainer.appendChild(divContainer)
+}
     })
 }
+
+
+const taskDetailBtn = document.querySelector('.task-details-btn');
+// taskDetailBtn.addEventListener('click', showDetails)
+
+console.log(taskDetailBtn)
 
 
 export {renderTasks}
